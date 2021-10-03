c2902969782e4473a74e0c1b8582ec099260b21e
danibix95 <dani95bissoli@gmail.com>
2021-10-03 17:29:39 +0200

    Added custom structured logger

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/danibix95/miabase
 go 1.16
 
 require (
+	github.com/danibix95/zeropino v0.3.0
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-chi/chi/v5 v5.0.3
+	github.com/go-chi/chi/v5 v5.0.4
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/rs/zerolog v1.25.0
 	github.com/stretchr/testify v1.7.0
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- miabase.go
+++ miabase.go
@@ -3,12 +3,16 @@ package miabase
 import (
 	"net/http"
 
+	"github.com/danibix95/zeropino"
+	zpstd "github.com/danibix95/zeropino/middlewares/std"
 	"github.com/go-chi/chi/v5"
+	"github.com/rs/zerolog"
 )
 
 type Service struct {
 	router *chi.Mux
 	Plugin *chi.Mux
+	Logger *zerolog.Logger
 }
 
 func NewService() *Service {
@@ -16,14 +20,23 @@ func NewService() *Service {
 	s.router = chi.NewRouter()
 	s.Plugin = chi.NewRouter()
 
+	logger, err := zeropino.Init(zeropino.InitOptions{Level: "info"})
+	if err != nil {
+		panic(err.Error())
+	}
+	s.Logger = logger
+
 	return s
 }
 
 func (s *Service) Start() {
 	s.router.Group(func(r chi.Router) {
+		r.Use(zpstd.RequestLogger(s.Logger, []string{"/-/"}))
+
 		r.Mount("/", s.Plugin)
 	})
 
 	server := &http.Server{Addr: "0.0.0.0:3000", Handler: s.router}
+	s.Logger.Info().Msg("Server listening on port :3000")
 	_ = server.ListenAndServe()
 }
