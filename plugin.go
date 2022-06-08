16610cae9fb75e513b96f1e21f085441ea902acb
Daniele Bissoli <daniele.bissoli@mia-platform.eu>
2022-06-08 16:52:19 +0200

    Added Inject method to test endpoint also in Plugin type

--- plugin.go
+++ plugin.go
@@ -11,6 +11,7 @@ type Plugin struct {
 	router *chi.Mux
 }
 
+// NewPlugin create a new plugin that groups a set of routes under it
 func NewPlugin(path string) *Plugin {
 	p := new(Plugin)
 	p.Path = path
@@ -19,6 +20,8 @@ func NewPlugin(path string) *Plugin {
 	return p
 }
 
+// AddRoute add a new endpoint to the plugin associated with the logic
+// that should be executed when the route is called
 func (p *Plugin) AddRoute(method, path string, handler http.HandlerFunc) {
 	switch method {
 	case "GET":
@@ -32,6 +35,11 @@ func (p *Plugin) AddRoute(method, path string, handler http.HandlerFunc) {
 	case "DELETE":
 		p.router.Delete(path, handler)
 	default:
-		panic("selected method it is not recognized")
+		panic("selected HTTP method is not recognized")
 	}
+}
+
+// Inject allow to test plugin routes by injecting the request and recording the response
+func (p *Plugin) Inject(w http.ResponseWriter, r *http.Request) {
+	p.router.ServeHTTP(w, r)
 }
